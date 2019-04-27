54d3aa47f46fa2e17a3a446a87290fb44ed96b78
Jacob <jjalex87@users.noreply.github.com>
2019-04-27 20:14:45 +0530

    Update stack_3_stack_using_ll.c

--- stack_3_stack_using_ll.c
+++ stack_3_stack_using_ll.c
@@ -20,6 +20,7 @@ node_t* insert_node(int data)
         node->next = NULL;
     } else {
         printf("malloc failed! Cannot insert node!\n");
+        return (NULL);
     }
 
     if (head == NULL) {
@@ -33,39 +34,58 @@ node_t* insert_node(int data)
     return (node);
 }
 
-void print_list(node_t *head)
+void print_list()
 {
-    while (head != NULL) {
-        printf("%d->", head->data);
-        head = head->next;
+    node_t *curr = head;
+    
+    if (curr == NULL) {
+        printf("Stack is empty! Nothing to print\n");
+        return;
+    }
+    
+    while (curr != NULL) {
+        printf("%d->", curr->data);
+        curr = curr->next;
     }
     printf("NULL\n");
 }
 
 int delete_node(node_t *node)
 {
-    node_t *temp_node = head;
-    node_t *prev_node = head;
-    node_t *next_node;
+    node_t *curr = head;
+    node_t *prev = head;
     int data;
-    int count = 0;
-
-    while (temp_node != NULL) {
-        if (temp_node == node) {
-            next_node = node->next;
-            data = node->data;
-            free(node);
-            break;
-        } else {
-            prev_node = temp_node;
-            temp_node = temp_node->next;
-            count++;
-        }
+    
+    if (node == NULL) {
+        return (-1);
     }
-    if (count == 0) {
-        head = next_node;
+    
+    if (node == head) {
+        data = node->data;
+        head = head->next;
+        free(node);
+        if (head == NULL) {
+            tail = NULL;
+        }
+        return data;
+    }
+
+    while (curr != NULL && curr != node) {
+        prev = curr;
+        curr = curr->next;
+    }
+    
+    if (curr == NULL) {
+        printf("ERROR: node not found!\n");
+        return (-1);
     } else {
-        prev_node->next = next_node;
+        data = curr->data;
+        prev->next = curr->next;
+        free(curr);
+        if (prev->next == NULL) {
+            tail = prev;
+        }
+        return data;
     }
 }
 
@@ -76,25 +96,78 @@ void push(int a)
 
 int pop()
 {
-    delete_node(tail);
+    int data = delete_node(tail);
+    if (data == -1) {
+        printf("Stack is empty! Nothing to pop!\n");
+    }
+    return (data);
 }
 
 void main()
 {
-    node_t *node1, *node2, *node3 = NULL;
-
-    head = insert_node(head, 3);
-
-    node1 = insert_node(head, 4);
-    node2 = insert_node(head, 5);
-    node3 = insert_node(head, 6);
-
-    print_list(head);
-
-    delete_node(&head, head);
-    //delete_node(&head, node1);
-    //delete_node(&head, node2);
-    delete_node(&head, node3);
-
-    print_list(head);
+    int i, data;
+    
+    print_list();
+    pop();
+    
+    push(100);
+    pop();
+    push(200);
+    print_list();
+    
+    for (i = 1; i <= 5; i++) {
+        push(i);
+        print_list();
+    }
+    
+    for (i = 0; i < 2; i++) {
+        printf("Pop 1 Element: %d\n", pop());
+        print_list();
+    }
+    
+    for (i = 6; i < 9; i++) {
+        push(i);
+        print_list();
+    }
+    
+    for (i = 0; i < 10; i++) {
+        data = pop();
+        if (data == -1) {
+            break;
+        }
+        printf("Pop 1 Element: %d\n", data);
+        print_list();
+    }
 }
+
+Output:
+Stack is empty! Nothing to print                                                                                                               
+Stack is empty! Nothing to pop!                                                                                                                
+200->NULL                                                                                                                                      
+200->1->NULL                                                                                                                                   
+200->1->2->NULL                                                                                                                                
+200->1->2->3->NULL                                                                                                                             
+200->1->2->3->4->NULL                                                                                                                          
+200->1->2->3->4->5->NULL                                                                                                                       
+Pop 1 Element: 5                                                                                                                               
+200->1->2->3->4->NULL                                                                                                                          
+Pop 1 Element: 4                                                                                                                               
+200->1->2->3->NULL                                                                                                                             
+200->1->2->3->6->NULL                                                                                                                          
+200->1->2->3->6->7->NULL                                                                                                                       
+200->1->2->3->6->7->8->NULL                                                                                                                    
+Pop 1 Element: 8                                                                                                                               
+200->1->2->3->6->7->NULL                                                                                                                       
+Pop 1 Element: 7                                                                                                                               
+200->1->2->3->6->NULL                                                                                                                          
+Pop 1 Element: 6                                                                                                                               
+200->1->2->3->NULL                                                                                                                             
+Pop 1 Element: 3                                                                                                                               
+200->1->2->NULL                                                                                                                                
+Pop 1 Element: 2                                                                                                                               
+200->1->NULL                                                                                                                                   
+Pop 1 Element: 1                                                                                                                               
+200->NULL                                                                                                                                      
+Pop 1 Element: 200                                                                                                                             
+Stack is empty! Nothing to print                                                                                                               
+Stack is empty! Nothing to pop! 
