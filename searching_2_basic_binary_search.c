f500d2acc9d71d9bbad1a9310235a75a1de6cd39
Jacob <jjalex87@users.noreply.github.com>
2019-04-27 22:49:45 +0530

    Update searching_2_basic_binary_search.c

--- searching_2_basic_binary_search.c
+++ searching_2_basic_binary_search.c
@@ -4,26 +4,30 @@
 
 int binary_search(int *arr, int low, int high, int data)
 {
-    int mid;
+    int mid, index = -1;
+    int comparisons = 0;
     
     while (low <= high) {
+        comparisons++;
         mid = (low + high)/2;
         if (data == arr[mid]) {
-            return (mid);
+            index =mid;
+            break;
         } else if (data > arr[mid]) {
             low = mid + 1;
         } else {
             high = mid - 1;
         }
     }
-    return (-1);
+    printf("Comparisons made: %d\n", comparisons);
+    return (index);
 }
 
 int main()
 {
     int arr[ARR_MAX_SIZE] = {33, 45, 48, 67, 74, 78, 84, 88, 90, 99};
     
-    int index = binary_search(arr, 0, (ARR_MAX_SIZE - 1), 45);
+    int index = binary_search(arr, 0, (ARR_MAX_SIZE - 1), 78);
     
     if (index == -1) {
         printf("Element not found!");
@@ -35,4 +39,5 @@ int main()
 }
 
 Output:
-Element found at index: 1
+Comparisons made: 3                                                                                                                            
+Element found at index: 5 
