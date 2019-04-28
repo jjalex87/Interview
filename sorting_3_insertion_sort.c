03f3266b61625c2744744d80e316591d5b3e9f59
Jacob <jjalex87@users.noreply.github.com>
2019-04-28 18:36:41 +0530

    Update sorting_3_insertion_sort.c

--- sorting_3_insertion_sort.c
+++ sorting_3_insertion_sort.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/*
+This is an in-place comparison-based sorting algorithm. 
+
+Here, a sub-list is maintained which is always sorted.
+For example, the lower part of an array is maintained to be sorted. An element which is to be 'insert'ed in 
+this sorted sub-list, has to find its appropriate place and then it has to be inserted there. 
+Hence the name, insertion sort.The array is searched sequentially and unsorted items are moved and inserted 
+into the sorted sub-list (in the same array). 
+
+This algorithm is not suitable for large data sets as its average
+and worst case complexity are of Ο(n2), where n is the number of items.
+*/
+
 #define MAX_ARR_SIZE    7
 int arr[MAX_ARR_SIZE] = {4,6,3,2,1,9,7};
 
